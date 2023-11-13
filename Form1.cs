f25b60a730e982c96aa642ab9f3dd3026cdfe881
Kauê Alcantara Rocha <118750523+garotodeprogramacao@users.noreply.github.com>
2023-11-13 20:25:57 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _25_05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void img_cima_Click(object sender, EventArgs e)
+        {
+            if (img_personagem.Left > 0)
+            {
+                img_personagem.Left = img_personagem.Left - 10;
+            }
+        }
+
+        private void img_esquerda_Click(object sender, EventArgs e)
+        {
+            if(img_personagem.Left > 0)
+            {
+                img_personagem.Left = img_personagem.Left - 10;
+            }
+        }
+
+        private void img_direita_Click(object sender, EventArgs e)
+        {
+            if(img_personagem.Left > 600)
+            {
+                img_personagem.Left = img_personagem.Left + 10;
+            }
+        }
+
+        private void img_baixo_Click(object sender, EventArgs e)
+        {
+            if(img_personagem.Left > 600)
+            {
+                img_personagem.Left = img_personagem.Left + 10;
+            }
+        }
+    }
+}
